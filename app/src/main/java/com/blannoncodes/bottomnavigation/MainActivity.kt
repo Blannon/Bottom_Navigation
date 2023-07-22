0990288f5ff0cc908368d5c5336b083bb49ff77d
Blannon <63664876+Blannon@users.noreply.github.com>
2023-07-22 05:30:25 +0300

    sweet bottom navigation

---
+++ app/src/main/java/com/blannoncodes/bottomnavigation/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.blannoncodes.bottomnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottom_navi)
+
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId){
+                R.id.bottom_home -> {
+                    replaceFragment(HomeFragment())
+                true
+            }
+
+                R.id.bottom_add-> {
+                  Toast.makeText(this, "Upload Image & Video", Toast.LENGTH_SHORT).show()
+                    true
+                }
+
+                R.id.bottom_search -> {
+                    replaceFragment(SearchFragment())
+                    true
+                }
+
+                R.id.bottom_profile -> {
+                    replaceFragment(ProfileFragment())
+                    true
+                }
+
+                R.id.bottom_reels -> {
+                    replaceFragment(ReelsFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+        replaceFragment(HomeFragment())
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit()
+    }
+}
